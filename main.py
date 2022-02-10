b3d3f0a77d86638f945c1685fd16be1663fef734
Joseph Pagadora <joseph.pagadora.c@gmail.com>
2022-02-10 14:01:57 -0800

    start with streamlit for web app

---
+++ main.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import tensorflow as tf
+import torch
+import numpy
+from predict import *
+
+#import matplotlib.pyplot as plt
+#import seaborn as sns
+
+
+#sns.set_theme(style="darkgrid")
+#sns.set()
+#st.title('Check Text for Political Bias')
+
+text_input = st.text_area('Input text here:') 
+
+
---
+++ predict.py
@@ -0,0 +1,17 @@
+import torch
+
+#from transformers import DistilBertTokenizerFast
+from transformers import DistilBertForSequenceClassification
+
+
+def load_model():
+	with torch.no_grad():
+		model = DistilBertForSequenceClassification.from_pretrained("distilbert-base-uncased")
+		model_dict = torch.load('./pytorch_model.bin', map_location = 'cpu')
+		model.load_state_dict(model_dict)
+		model = model.eval()
+		return model
+
+model = load_model()
+
+
