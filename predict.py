eaff1bac77c109fc417cdc1ff5f66b30dffa1e43
Tassos Sapalidis <66144841+tassossapalidis@users.noreply.github.com>
2022-02-10 21:56:25 -0800

    push mvp v1

--- predict.py
+++ predict.py
@@ -1,10 +1,12 @@
 import torch
 
-#from transformers import DistilBertTokenizerFast
+from transformers import DistilBertTokenizer
 from transformers import DistilBertForSequenceClassification
 
-
 def load_model():
+	'''
+	Loads DistilBert model with our parameters
+	'''
 	with torch.no_grad():
 		model = DistilBertForSequenceClassification.from_pretrained("distilbert-base-uncased")
 		model_dict = torch.load('./pytorch_model.bin', map_location = 'cpu')
@@ -13,7 +15,18 @@ def load_model():
 		return model
 
 def score_bias(model, text_input):
-	pass
+	'''
+	Evaluate input text using our model
+	Output: probability for each class
+	'''
+	tokenizer = DistilBertTokenizer.from_pretrained("distilbert-base-uncased")
 
-model = load_model()
+	model_input = tokenizer(text_input,
+ 	          	stride=128,
+ 	          	truncation=True,
+ 	          	max_length=384,
+ 	          	padding=True,
+				return_tensors='pt')
 
+	score = model(**model_input)
+	return score
