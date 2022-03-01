e06e624dfbc5b043821102c02beb8fc7bc5a18ba
Joseph Pagadora <joseph.pagadora.c@gmail.com>
2022-02-28 22:20:38 -0800

    predictor version

--- backend/predictor.py
+++ backend/predictor.py
@@ -1,7 +1,52 @@
+import argparse
 import numpy as np
 import os
-from predict import *
-import utils
+import torch
+
+from transformers import DistilBertTokenizer
+from transformers import DistilBertForSequenceClassification
+from transformers import AutoConfig
+from newspaper import Article
+
+def get_content(url):
+    '''
+    Returns the title and content of news article at the specified url
+    input: URL (as string)
+    output: title of article, article content body (as strings)
+    '''
+    article = Article(url)
+    article.download()
+    article.parse()
+    return article.title.strip().replace('\n',''), article.text.strip().replace('\n','')
+
+def load_model(model_dir):
+    '''
+    Loads DistilBert model with our parameters
+    '''
+    with torch.no_grad():
+        config = AutoConfig.from_pretrained("distilbert-base-uncased")
+        model = DistilBertForSequenceClassification(config)
+        model_dict = torch.load(model_dir, map_location = 'cpu')
+        model.load_state_dict(model_dict)
+        model = model.eval()
+        return model
+
+def score_bias(model, text_input):
+    '''
+    Evaluate input text using our model
+    Output: probability for each class
+    '''
+    tokenizer = DistilBertTokenizer.from_pretrained("distilbert-base-uncased")
+
+    model_input = tokenizer(text_input,
+                stride=128,
+                truncation=True,
+                max_length=384,
+                padding=True,
+                return_tensors='pt')
+
+    score = model(**model_input)
+    return score
 
 class Preprocessor(object):
 
@@ -9,7 +54,10 @@ class Preprocessor(object):
         """
         Preprocesses text input
         """
-        return prepare_inference_input(url, 100)
+        text_input = get_content(url)
+        text_input = text_input[0] + '[SEP]' + text_input[1]
+
+        return text_input
 
 class MyPredictor(object):
     """An example Predictor for an AI Platform custom prediction routine."""
