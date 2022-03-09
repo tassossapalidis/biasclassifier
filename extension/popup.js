c7bfa295174f2e8acdd0f869d2e4c70b1edc9237
Tassos Sapalidis <66144841+tassossapalidis@users.noreply.github.com>
2022-03-08 23:52:53 -0800

    ui edits

--- extension/popup.js
+++ extension/popup.js
@@ -6,6 +6,56 @@ let isloaded = false;
 
 let api_url = 'https://us-west1-cs-329-bias-classifier.cloudfunctions.net/polarity_func-2';
 
+var gaugeOpts = {
+    // color configs
+    colorStart: "#6fadcf",
+    colorStop: void 0,
+    gradientType: 0,
+    strokeColor: "#e0e0e0",
+    generateGradient: true,
+    percentColors: [[0.0, "#a9d70b" ], [0.50, "#f9c802"], [1.0, "#ff0000"]],
+    // customize pointer
+    pointer: {
+      length: 0.8,
+      strokeWidth: 0.035,
+      iconScale: 1.0
+    },
+    // static zones
+    staticZones: [
+      {strokeStyle: "#2E73E6", min: 0, max: .1},
+      {strokeStyle: "#3BA8E7", min: .1, max: .25},
+      {strokeStyle: "#FDE25D", min: .25, max: .75},
+      {strokeStyle: "#FB607E", min: .75, max: .9},
+      {strokeStyle: "#E92A39", min: .9, max: 1}
+    ],
+    // render ticks
+    // renderTicks: {
+    //   divisions: 5,
+    //   divWidth: 1.1,
+    //   divLength: 0.7,
+    //   divColor: "#333333",
+    //   subDivisions: 3,
+    //   subLength: 0.5,
+    //   subWidth: 0.6,
+    //   subColor: "#666666"
+    // },
+    // the span of the gauge arc
+    angle: 0.15,
+    // line thickness
+    lineWidth: 0.44,
+    // radius scale
+    radiusScale: 1.0,
+    // font size
+    fontSize: 40,
+    // if false, max value increases automatically if value > maxValue
+    limitMax: false,
+    // if true, the min value of the gauge will be fixed
+    limitMin: false,
+    // High resolution support
+    highDpiSupport: true
+};
+
+
 document.addEventListener("DOMContentLoaded", function() {
     isloaded = true;
 })
@@ -27,11 +77,12 @@ function amendHTML(tabs) {
     runPolarity.textContent = ``;
     runPolarity.classList.add("button--loading");
     removeAllChildNodes(divResults1)
-    removeAllChildNodes(divResults2);
+    // removeAllChildNodes(divResults2);
 
     let title = tabs[0].title;
     //let results_child = document.createTextNode('Model results here');
-    let title_child = document.createTextNode('Article title: '.concat(title));
+    bold = document.createElement('strong')
+    let title_child = document.createTextNode(title);
     //divResults1.appendChild(results_child);
 
     removeAllChildNodes(divIsRun);
@@ -41,7 +92,6 @@ function amendHTML(tabs) {
 // helper function to remove nodes from divIsRun
 function removeAllChildNodes(parent) {
     while (parent.firstChild) {
-        console.log(2);
         parent.removeChild(parent.firstChild);
     }
 }
@@ -57,7 +107,6 @@ async function getPrediction(tab) {
     })
     .then(response => {return response.json()})
     .then(data => {
-        console.log(data)
         let median_conf = data['med_conf'];
         let max_conf = data['max_conf'];
         let median_class = data['med_class'];
@@ -67,13 +116,37 @@ async function getPrediction(tab) {
         if (median_class == 1) median_leaning = "conservative"
         if (maximum_class == 1) maximum_leaning = "conservative"
 
-        let median_child = document.createTextNode('This article\'s median bias is '.concat(Math.round((median_conf * 100)).toString()).concat('% ').concat(median_leaning));
-        let maximum_child = document.createTextNode('This article\'s maximum bias is '.concat(Math.round((max_conf * 100)).toString()).concat('% ').concat(maximum_leaning));
+        let bias_display = '';
+        if (median_leaning == 'conservative') {
+            if (median_conf >= .9) {
+                bias_display = 'is very conservative.'
+            }
+            else if (median_conf >= .75) {
+                bias_display = 'leans conservative.'
+            }
+            else {
+                bias_display = 'is politically neutral.'
+            }
+        }
+        else {
+            if (median_conf >= .9) {
+                bias_display = 'is very liberal.'
+            }
+            else if (median_conf >= .75) {
+                bias_display = 'leans liberal.'
+            }
+            else {
+                bias_display = 'is politically neutral.'
+            }
+        }
+        // let median_child = document.createTextNode('This article\'s median bias is '.concat(Math.round((median_conf * 100)).toString()).concat('% ').concat(median_leaning));
+        let median_child = document.createTextNode('This article '.concat(bias_display));
+        // let maximum_child = document.createTextNode('This article\'s maximum bias is '.concat(Math.round((max_conf * 100)).toString()).concat('% ').concat(maximum_leaning));
 
         removeAllChildNodes(divResults1);
-        removeAllChildNodes(divResults2);
+        // removeAllChildNodes(divResults2);
         divResults1.appendChild(median_child)
-        divResults2.appendChild(maximum_child);
+        // divResults2.appendChild(maximum_child);
 
         let attr_sentences = data['bias_sentences']
         let substring = attr_sentences[0].substring(45, Math.min(95, attr_sentences[0].length))
@@ -91,17 +164,36 @@ async function getPrediction(tab) {
             }
         }
 
-        console.log(selectors)
-
         let msg = {
             sels : selectors,
             bias_type : maximum_leaning,
             max_conf : max_conf,
         }
 
-        console.log(tab.id)
         chrome.tabs.sendMessage(tab.id,msg);
 
+        var opts = {
+            // options here
+        };
+
+        var target = document.getElementById('gauge'); 
+        var gauge = new Gauge(target).setOptions(gaugeOpts);
+
+        // document.getElementById("results-textfield").className = "results-textfield";
+        // gauge.setTextField(document.getElementById("results-textfield"));
+        // document.getElementById("results-textfield").appendChild(median_child);
+
+        gauge.maxValue = 1;
+        gauge.setMinValue(0); 
+        let gauge_value = 0.5;
+        if (median_leaning == 'conservative') {
+            gauge_value = median_conf
+        }
+        else {
+            gauge_value = 1 - median_conf
+        }
+        gauge.set(gauge_value);
+        gauge.animationSpeed = 32
     })
     .catch(error => console.error(error));
 
